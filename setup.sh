9b6e350ece03cb4c66094f3fc6a31ddebc877bec
Ranju <ranjujohn@gmail.com>
2018-10-04 10:56:30 +0200

    Updated the upgrade script

--- setup.sh
+++ setup.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 export KUBECONFIG=.kube/config_infinity
 export KOPS_STATE_STORE=s3://$S3Bucket
-# kops get $Cluster-Name --state=s3://$S3-Bucket -o yaml > $Cluster-Name.yaml
+git clone https://github.com/ranjujohn/k8s.git
+kops get $ClusterName --state=s3://$S3-Bucket -o yaml > $ClusterName.yaml
+currentversion=$(cat $ClusterName.yaml  | grep kubernetesVersion | awk '{print $2}')
+newversion=$(cat k8s/version | grep Version | awk '{print $2}')
+sed -i "s/$currentversion/$newversion/g" $ClusterName.yaml
 kops replace -f $ClusterName.yaml
 kops update cluster $ClusterName --yes
 kops rolling-update cluster $ClusterName --yes
